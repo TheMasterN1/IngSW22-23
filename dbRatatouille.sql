6505c8ee727923f569242ac5397b4f5d11a5df9e
Ciro Cancello <57488721+Cirocancello@users.noreply.github.com>
2023-05-09 22:36:43 +0200

    Add files via upload

--- dbRatatouille.sql
+++ dbRatatouille.sql
@@ -22,9 +22,7 @@ CREATE TABLE IF NOT EXISTS `ingrediente` (
   `nome` varchar(50) NOT NULL,
   `ingredienteId` int(11) NOT NULL AUTO_INCREMENT,
   `quantita` int(11) NOT NULL,
-  `prodotID` int(11) NOT NULL,
-  PRIMARY KEY (`ingredienteId`),
-  KEY `FK1_prodotID` (`prodotID`)
+  PRIMARY KEY (`ingredienteId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- L’esportazione dei dati non era selezionata.
@@ -32,11 +30,20 @@ CREATE TABLE IF NOT EXISTS `ingrediente` (
 -- Dump della struttura di tabella ratatouille.menu
 CREATE TABLE IF NOT EXISTS `menu` (
   `menuID` int(11) NOT NULL AUTO_INCREMENT,
-  `prodottoID` int(11) NOT NULL,
   PRIMARY KEY (`menuID`),
-  KEY `menuID` (`menuID`),
-  KEY `prodottoID` (`prodottoID`),
-  CONSTRAINT `FK1_prodottoID` FOREIGN KEY (`prodottoID`) REFERENCES `prodotto` (`prodottoID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `menuID` (`menuID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella ratatouille.ordinazione
+CREATE TABLE IF NOT EXISTS `ordinazione` (
+  `ordineID` int(11) DEFAULT NULL,
+  `prodottoID` int(11) DEFAULT NULL,
+  KEY `ordineID` (`ordineID`),
+  KEY `FK__prodotto` (`prodottoID`),
+  CONSTRAINT `FK__ordine` FOREIGN KEY (`ordineID`) REFERENCES `ordine` (`ordineId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK__prodotto` FOREIGN KEY (`prodottoID`) REFERENCES `prodotto` (`prodottoID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- L’esportazione dei dati non era selezionata.
@@ -53,6 +60,18 @@ CREATE TABLE IF NOT EXISTS `ordine` (
 
 -- L’esportazione dei dati non era selezionata.
 
+-- Dump della struttura di tabella ratatouille.portata
+CREATE TABLE IF NOT EXISTS `portata` (
+  `menuID` int(11) DEFAULT NULL,
+  `prodottoID` int(11) DEFAULT NULL,
+  KEY `FK_1` (`menuID`),
+  KEY `FK_2` (`prodottoID`),
+  CONSTRAINT `FK_1` FOREIGN KEY (`menuID`) REFERENCES `menu` (`menuID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_2` FOREIGN KEY (`prodottoID`) REFERENCES `prodotto` (`prodottoID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- L’esportazione dei dati non era selezionata.
+
 -- Dump della struttura di tabella ratatouille.prodotto
 CREATE TABLE IF NOT EXISTS `prodotto` (
   `costo` float NOT NULL,
@@ -62,17 +81,20 @@ CREATE TABLE IF NOT EXISTS `prodotto` (
   `prodottoID` int(11) NOT NULL AUTO_INCREMENT,
   `quantita` int(11) NOT NULL,
   `categoria` enum('Primo','Secondo','Contorno','Dolce','Antipasto','Bibita') NOT NULL,
-  `menuID` int(11) NOT NULL,
-  `ordineID` int(11) NOT NULL,
-  `ingredID` int(11) DEFAULT NULL,
   PRIMARY KEY (`prodottoID`),
-  KEY `prodottoID` (`prodottoID`),
-  KEY `FK1_oridneID` (`ordineID`),
-  KEY `FK2_prodotto_menu` (`menuID`),
-  KEY `FK3_ingredID` (`ingredID`),
-  CONSTRAINT `FK1_oridneID` FOREIGN KEY (`ordineID`) REFERENCES `ordine` (`ordineId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK2_prodotto_menu` FOREIGN KEY (`menuID`) REFERENCES `menu` (`menuID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK3_ingredID` FOREIGN KEY (`ingredID`) REFERENCES `ingrediente` (`ingredienteId`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  KEY `prodottoID` (`prodottoID`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- L’esportazione dei dati non era selezionata.
+
+-- Dump della struttura di tabella ratatouille.ricetta
+CREATE TABLE IF NOT EXISTS `ricetta` (
+  `prodottoID` int(11) DEFAULT NULL,
+  `ingredienteID` int(11) DEFAULT NULL,
+  KEY `FK` (`prodottoID`),
+  KEY `FK1` (`ingredienteID`),
+  CONSTRAINT `FK` FOREIGN KEY (`prodottoID`) REFERENCES `prodotto` (`prodottoID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK1` FOREIGN KEY (`ingredienteID`) REFERENCES `ingrediente` (`ingredienteId`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- L’esportazione dei dati non era selezionata.
