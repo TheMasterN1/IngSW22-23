8b551d8b5a8a37e6d835acd51508f6a9878e472a
Ciro Cancello <57488721+Cirocancello@users.noreply.github.com>
2023-09-29 22:37:23 +0200

    Add files via upload

--- dbRatatouille.sql
+++ dbRatatouille.sql
@@ -53,6 +53,7 @@ CREATE TABLE IF NOT EXISTS `menu` (
 CREATE TABLE IF NOT EXISTS `ordinazione` (
   `ordineID` int(11) DEFAULT NULL,
   `prodottoID` int(11) DEFAULT NULL,
+  `quantita` int(11) DEFAULT NULL,
   KEY `ordineID` (`ordineID`),
   KEY `FK__prodotto` (`prodottoID`),
   CONSTRAINT `FK__ordine` FOREIGN KEY (`ordineID`) REFERENCES `ordine` (`ordineId`) ON DELETE NO ACTION ON UPDATE NO ACTION,
@@ -65,25 +66,10 @@ CREATE TABLE IF NOT EXISTS `ordinazione` (
 CREATE TABLE IF NOT EXISTS `ordine` (
   `ordineId` int(11) NOT NULL AUTO_INCREMENT,
   `costoOrdine` int(11) NOT NULL,
-  `prodId` int(11) NOT NULL,
   `tavoloID` int(11) NOT NULL,
   PRIMARY KEY (`ordineId`),
-  KEY `FK1_prodID` (`prodId`),
   KEY `FK2_tavoloID` (`tavoloID`),
-  CONSTRAINT `FK1_prodID` FOREIGN KEY (`prodId`) REFERENCES `prodotto` (`prodottoID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `FK2_tavoloID` FOREIGN KEY (`tavoloID`) REFERENCES `tavolo` (`tavoloID`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- L’esportazione dei dati non era selezionata.
-
--- Dump della struttura di tabella ratatouille.portata
-CREATE TABLE IF NOT EXISTS `portata` (
-  `menuID` int(11) DEFAULT NULL,
-  `prodottoID` int(11) DEFAULT NULL,
-  KEY `FK_1` (`menuID`),
-  KEY `FK_2` (`prodottoID`),
-  CONSTRAINT `FK_1` FOREIGN KEY (`menuID`) REFERENCES `menu` (`menuID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_2` FOREIGN KEY (`prodottoID`) REFERENCES `prodotto` (`prodottoID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- L’esportazione dei dati non era selezionata.
@@ -165,11 +151,69 @@ CREATE TABLE IF NOT EXISTS `visualizzazione` (
 
 -- L’esportazione dei dati non era selezionata.
 
+-- Dump della struttura di trigger ratatouille.bacheca_insert
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `bacheca_insert` AFTER INSERT ON `bacheca` FOR EACH ROW BEGIN
+   DECLARE X INTEGER; 
+
+	FOR X IN(SELECT id FROM utente WHERE id <> NEW.IDutente)	DO
+		INSERT INTO visualizzazione(id, visualizzato) VALUES(NEW.IDutente, FALSE);
+	END FOR;
+	
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dump della struttura di trigger ratatouille.ordinazione_after_delete
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `ordinazione_after_delete` AFTER DELETE ON `ordinazione` FOR EACH ROW BEGIN
+	DECLARE X FLOAT DEFAULT (SELECT costo FROM prodotto WHERE prodottoID = OLD.prodottoID);
+	UPDATE ordine 
+	SET costoOrdine = costoOrdine - X
+	WHERE ordineID = OLD.ordineID;
+	
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dump della struttura di trigger ratatouille.ordinazione_after_insert
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `ordinazione_after_insert` AFTER INSERT ON `ordinazione` FOR EACH ROW BEGIN
+	DECLARE X FLOAT DEFAULT (SELECT costo FROM prodotto WHERE prodottoID = NEW.prodottoID);
+	UPDATE ordine 
+	SET costoOrdine = costoOrdine + X
+	WHERE ordineID = NEW.ordineID;
+	
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
 -- Dump della struttura di trigger ratatouille.ricetta_after_delete
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
 DELIMITER //
 CREATE TRIGGER `ricetta_after_delete` AFTER DELETE ON `ricetta` FOR EACH ROW BEGIN
  DELETE FROM prodotto WHERE OLD.prodottoID = prodotto.prodottoID;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dump della struttura di trigger ratatouille.tavolo_after_delete
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `tavolo_after_delete` AFTER DELETE ON `tavolo` FOR EACH ROW BEGIN
+	DELETE FROM ordine WHERE tavoloID = OLD.tavoloID;
+END//
+DELIMITER ;
+SET SQL_MODE=@OLDTMP_SQL_MODE;
+
+-- Dump della struttura di trigger ratatouille.tavolo_after_insert
+SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
+DELIMITER //
+CREATE TRIGGER `tavolo_after_insert` AFTER INSERT ON `tavolo` FOR EACH ROW BEGIN
+	INSERT INTO ordine(tavoloID) VALUES (NEW.tavoloID);
 END//
 DELIMITER ;
 SET SQL_MODE=@OLDTMP_SQL_MODE;
