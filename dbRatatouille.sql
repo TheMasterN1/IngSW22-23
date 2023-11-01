24136bdc030ed79f27744c1eb0ede9e1756776bc
TheMasterN1 <91351826+TheMasterN1@users.noreply.github.com>
2023-11-01 14:58:14 +0100

    Add files via upload

--- dbRatatouille.sql
+++ dbRatatouille.sql
@@ -30,7 +30,10 @@ CREATE TABLE IF NOT EXISTS `bacheca` (
   CONSTRAINT `sender` FOREIGN KEY (`IDutente`) REFERENCES `utente` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.bacheca: ~3 rows (circa)
+INSERT IGNORE INTO `bacheca` (`IDnotifica`, `oggetto`, `testo`, `IDutente`) VALUES
+	(23, 'Saluto', 'ciao pisello come va ti voglio bene', 2),
+	(28, 'Avviso Limite Superato', 'L\'ingrediente Pancia ha raggiunto il limite minimo, si prega di fare rifornimento al più presto', NULL);
 
 -- Dump della struttura di tabella ratatouille.ingrediente
 CREATE TABLE IF NOT EXISTS `ingrediente` (
@@ -40,7 +43,9 @@ CREATE TABLE IF NOT EXISTS `ingrediente` (
   PRIMARY KEY (`ingredienteId`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.ingrediente: ~2 rows (circa)
+INSERT IGNORE INTO `ingrediente` (`nome`, `ingredienteId`, `quantita`) VALUES
+	('Pancia', 2, 4);
 
 -- Dump della struttura di tabella ratatouille.menu
 CREATE TABLE IF NOT EXISTS `menu` (
@@ -49,7 +54,7 @@ CREATE TABLE IF NOT EXISTS `menu` (
   KEY `menuID` (`menuID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.menu: ~0 rows (circa)
 
 -- Dump della struttura di tabella ratatouille.ordinazione
 CREATE TABLE IF NOT EXISTS `ordinazione` (
@@ -62,7 +67,7 @@ CREATE TABLE IF NOT EXISTS `ordinazione` (
   CONSTRAINT `FK__prodotto` FOREIGN KEY (`prodottoID`) REFERENCES `prodotto` (`prodottoID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.ordinazione: ~0 rows (circa)
 
 -- Dump della struttura di tabella ratatouille.ordine
 CREATE TABLE IF NOT EXISTS `ordine` (
@@ -74,8 +79,6 @@ CREATE TABLE IF NOT EXISTS `ordine` (
   CONSTRAINT `FK2_tavoloID` FOREIGN KEY (`tavoloID`) REFERENCES `tavolo` (`tavoloID`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
-
 -- Dump della struttura di tabella ratatouille.portata
 CREATE TABLE IF NOT EXISTS `portata` (
   `menuID` int(11) DEFAULT NULL,
@@ -86,7 +89,7 @@ CREATE TABLE IF NOT EXISTS `portata` (
   CONSTRAINT `FK_2` FOREIGN KEY (`prodottoID`) REFERENCES `prodotto` (`prodottoID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.portata: ~0 rows (circa)
 
 -- Dump della struttura di tabella ratatouille.prodotto
 CREATE TABLE IF NOT EXISTS `prodotto` (
@@ -100,7 +103,9 @@ CREATE TABLE IF NOT EXISTS `prodotto` (
   KEY `prodottoID` (`prodottoID`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1000121222 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.prodotto: ~1 rows (circa)
+INSERT IGNORE INTO `prodotto` (`costo`, `nome`, `descrizione`, `prodottoID`, `categoria`, `allergeni`) VALUES
+	(0.5, 'Gelato', 'Gelato caldo', 1, 'Primo', 'Uova');
 
 -- Dump della struttura di tabella ratatouille.ricetta
 CREATE TABLE IF NOT EXISTS `ricetta` (
@@ -113,7 +118,9 @@ CREATE TABLE IF NOT EXISTS `ricetta` (
   CONSTRAINT `FK1` FOREIGN KEY (`ingredienteID`) REFERENCES `ingrediente` (`ingredienteId`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.ricetta: ~0 rows (circa)
+INSERT IGNORE INTO `ricetta` (`prodottoID`, `ingredienteID`, `quantità`) VALUES
+	(1, 2, 4);
 
 -- Dump della struttura di tabella ratatouille.sala
 CREATE TABLE IF NOT EXISTS `sala` (
@@ -121,7 +128,9 @@ CREATE TABLE IF NOT EXISTS `sala` (
   PRIMARY KEY (`salaID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.sala: ~2 rows (circa)
+INSERT IGNORE INTO `sala` (`salaID`) VALUES
+	(1);
 
 -- Dump della struttura di tabella ratatouille.tavolo
 CREATE TABLE IF NOT EXISTS `tavolo` (
@@ -133,7 +142,16 @@ CREATE TABLE IF NOT EXISTS `tavolo` (
   CONSTRAINT `FK1_salaID` FOREIGN KEY (`salaID`) REFERENCES `sala` (`salaID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.tavolo: ~9 rows (circa)
+INSERT IGNORE INTO `tavolo` (`tavoloID`, `numeroPosti`, `salaID`) VALUES
+	(20, 3, 1),
+	(21, 5, 1),
+	(22, 1, 1),
+	(23, 5, 1),
+	(24, 5, 1),
+	(25, 4, 1),
+	(26, 5, 1),
+	(27, 5, 1);
 
 -- Dump della struttura di tabella ratatouille.utente
 CREATE TABLE IF NOT EXISTS `utente` (
@@ -148,7 +166,11 @@ CREATE TABLE IF NOT EXISTS `utente` (
   CONSTRAINT `FK1_IDsala` FOREIGN KEY (`IDsala`) REFERENCES `sala` (`salaID`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.utente: ~3 rows (circa)
+INSERT IGNORE INTO `utente` (`username`, `password`, `id`, `ruolo`, `IDsala`, `nuovoUtente`) VALUES
+	('q', 'q', 2, 'Amministratore', NULL, 'N'),
+	('asd', 'asd', 3, 'Addetto_Cucina', NULL, 'N'),
+	('c', 'ciccia', 4, 'Addetto_Sala', 1, 'N');
 
 -- Dump della struttura di tabella ratatouille.visualizzazione
 CREATE TABLE IF NOT EXISTS `visualizzazione` (
@@ -161,7 +183,14 @@ CREATE TABLE IF NOT EXISTS `visualizzazione` (
   CONSTRAINT `FK2Notifica` FOREIGN KEY (`IDnotifica`) REFERENCES `bacheca` (`IDnotifica`) ON DELETE CASCADE ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
 
--- L’esportazione dei dati non era selezionata.
+-- Dump dei dati della tabella ratatouille.visualizzazione: ~6 rows (circa)
+INSERT IGNORE INTO `visualizzazione` (`ID`, `IDnotifica`, `visualizzato`) VALUES
+	(3, 23, 'N'),
+	(4, 23, 'N'),
+	(2, 23, 'Y'),
+	(2, 28, 'Y'),
+	(3, 28, 'N'),
+	(4, 28, 'N');
 
 -- Dump della struttura di trigger ratatouille.bacheca_insert
 SET @OLDTMP_SQL_MODE=@@SQL_MODE, SQL_MODE='STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION';
